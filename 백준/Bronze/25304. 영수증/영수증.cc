70ac1ed009dc77caf5409b05dd2467478604733d
수효 Soohyo <69195905+Dynamic-Seagull@users.noreply.github.com>
2023-09-22 22:37:01 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x,n,a,b;
+    int sum = 0;
+    cin >> x >> n;
+    
+    for(int i=0; i<n; i++){
+        cin >> a >> b;
+        sum += (a*b);
+    }
+    
+    if(x==sum) printf("Yes");
+    else printf("No");
+}
