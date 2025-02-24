9f1b67a655c46b01e3de2bff3e6bfb776edc76af
수효 Soohyo <69195905+Dynamic-Seagull@users.noreply.github.com>
2025-02-24 20:55:40 +0900

    [Silver V] Title: 단어 정렬, Time: 560 ms, Memory: 4108 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool shorter(const string& a, const string& b)
+{
+    if (a.length() == b.length())
+        return a < b;
+    return a.length() < b.length();
+}
+
+int main()
+{
+    int n; //단어개수
+    cin >> n;
+
+    vector<string> word(n);
+    for (int i = 0; i < n; i++)
+        cin >> word[i];
+
+    sort(word.begin(), word.end(), shorter); //오름차순 정렬
+
+    word.erase(unique(word.begin(), word.end()), word.end()); //중복제거
+
+    n = word.size();
+
+    for (int i = 0; i < n; i++)
+        cout << word[i] << endl;
+    
+    return 0;
+}
