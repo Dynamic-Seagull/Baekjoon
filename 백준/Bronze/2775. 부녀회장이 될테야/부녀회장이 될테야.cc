2fb9c2eddbbdef59fad9ffa512e83cf2922c1b7b
수효 Soohyo <69195905+Dynamic-Seagull@users.noreply.github.com>
2025-02-25 19:25:12 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int arr[15][15] = { 0, };
+	int T, k, n; //테스트케이스, 층, 호수
+	cin >> T;
+
+	for (int i = 0; i < 15; i++) //1층 세팅
+		arr[0][i] = i;
+
+	for (int i = 1; i < 15; i++) //각 층 세팅
+		for (int j = 1; j < 15; j++)
+			arr[i][j] = arr[i - 1][j] + arr[i][j - 1]; //아래층 같은호수 + 같은층 이전호수
+	
+	for (int i = 0; i < T; i++)
+	{
+		cin >> k >> n;
+		cout << arr[k][n] << "\n";
+	}
+}
