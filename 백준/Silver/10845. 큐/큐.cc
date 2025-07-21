f0149591d83073927b2a6430d5432319fe5722bb
김연경 Kim.Y.G <69195905+Dynamic-Seagull@users.noreply.github.com>
2025-07-21 11:35:47 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin >> n;
+    queue<int> q;
+    
+    while(n--)
+    {
+        string cmd;
+        cin >> cmd;
+        
+        if (cmd == "push") 
+        {
+            int x;
+            cin >> x;
+            q.push(x);
+        } 
+        else if (cmd == "pop") 
+        {
+            if (q.empty()) 
+                cout << -1 << '\n';
+            else 
+            {
+                cout << q.front() << '\n';
+                q.pop();
+            }
+        } 
+        else if (cmd == "size") 
+        {
+            cout << q.size() << '\n';
+        } 
+        else if (cmd == "empty") 
+        {
+            cout << (q.empty() ? 1 : 0) << '\n';
+        } 
+        else if (cmd == "front") 
+        {
+            cout << (q.empty() ? -1 : q.front()) << '\n';
+        } 
+        else if (cmd == "back") 
+        {
+            cout << (q.empty() ? -1 : q.back()) << '\n';
+        }
+    }
+}
